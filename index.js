dbcd05521457ffedb2a77fd67fab61f4a51add4b
kyleburgin1 <kyleburginn@gmail.com>
2024-01-12 09:26:46 +1100

    changed buttons to even listeners

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-
+const closeBtn = document.querySelector(".close-btn");
 const sideNav = document.getElementById("my-sidenav");
+const menuBtn = document.querySelector(".menu-button");
 
-function openNav(){
+menuBtn.addEventListener("click", function(){
     sideNav.style.width = "250px";
-};
+})
 
-function closeNav(){
+closeBtn.addEventListener("click", function(){
     sideNav.style.width = "0";
-};
+});
+
+
+/*function openNav(){
+    sideNav.style.width = "250px";
+};*/
+
+/*function closeNav(){
+    sideNav.style.width = "0";
+};*/
