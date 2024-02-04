7a06323079aba2cc48f9ebe7e43207f3c68eac72
kyleburgin1 <kyleburginn@gmail.com>
2024-02-04 19:50:08 +1100

    added border on nav open

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const closeBtn = document.querySelector(".close-btn");
 const sideNav = document.getElementById("my-sidenav");
 const menuBtn = document.querySelector(".menu-button");
 
+
+
+
 menuBtn.addEventListener("click", function(){
     sideNav.style.width = "250px";
-})
+    sideNav.style.borderLeft = "8px double #ffffff";
+    
+});
 
 closeBtn.addEventListener("click", function(){
     sideNav.style.width = "0";
+    sideNav.style.borderLeft = "none";
 });
 
 
